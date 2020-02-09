1d4906e363bcfe7dda862176f99df90d1867bd73
hansensen <hanjie1030@gmail.com>
2020-02-09 17:15:39 +0800

    replace hardlim with iteration

--- Homework1/Q3/percep.m
+++ Homework1/Q3/percep.m
@@ -1,18 +1,22 @@
 function [w_b, w_hist] = percep(w_b_init, x, d, lr, iteration)
 %PERCEP Summary of this function goes here
 %   Detailed explanation goes here
-    [n, m] = size(x)
+    [n, m] = size(x);
     x = [ones(1, m); x];
     [len, ~] = size(x);
     w_hist = zeros([len, iteration]);
     w_b = w_b_init;
     for i = 1: iteration
-        v = w_b * x;
-        y = hardlim(v);
+        v = w_b * x
+        y = ones(1, length(v))
+        for j = 1: length(v)
+            if (v(j) < 0)
+                y(j) = 0;
+            end
+        end
         e = d - y;
         % update w
-        w_b = w_b + lr * (d -y ) * x';
+        w_b = w_b + lr * e * x';
         w_hist(:, i) = w_b;
     end
 end
-
