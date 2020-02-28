afc3f212503f935bee51bc04cf1c76da82f5bfdb
hansensen <hanjie1030@gmail.com>
2020-02-28 15:46:42 +0800

    complete q3

--- Homework2/q3.m
+++ Homework2/q3.m
@@ -29,7 +29,6 @@ fprintf("\nWithout PCA\n");
 
 for i = 1 : length(lst_resize_ratio)
     scale = 1 / lst_resize_ratio(i);
-    disp(scale)
     [xtrain, ytrain] = load_data(train_path, scale, false);
     [xval, yval] = load_data(val_path, scale, false);
 
@@ -55,36 +54,40 @@ for i = 1 : length(lst_resize_ratio)
 end
 
 %% c) 
+fprintf("\nc\n");
 [xtrain, ytrain] = load_data(train_path, 1, false);
 [xval, yval] = load_data(val_path, 1, false);
+accu_train_lst = [];
+accu_val_lst = [];
 
-% for n = [1:10, 20, 50, 100]
-for n = [10]
+for n = [1:10, 20, 50, 100]
     [net, tr, accu_train, accu_val] = train_batch(n, xtrain, ytrain, xval,...
     yval, 1000, 0);
+    accu_train_lst = cat(1, accu_train_lst, accu_train);
+    accu_val_lst = cat(1, accu_val_lst, accu_val);
 
     fprintf("Hidden Neurons: %d\nTraining Acc: %f, Val Acc: %f\n", ...
     n, accu_train, accu_val);
 end
 
-%% Q3d
+%%
+plot(1:1:13, accu_train_lst, 'b')
+hold on;
+plot(1:1:13, accu_val_lst, 'r')
 
+%% Q3d
 fprintf("d)\n")
 [xtrain, ytrain] = load_data(train_path,1,false);
 [xval, yval] = load_data(val_path,1,false);
 
-% the result is better with regulatization
-
-lst_reg = [0, .1, .2, .3, .4, .5, .6, .8, .9, 1];
+lst_reg = [.1, .2, .3, .4, .5, .6, .8, .9, 1];
 n = 10;
-for n = [1:10, 20, 50, 100]
-    fprintf("\nHidden Neurions: %d\n", n)
-    for reg = lst_reg
-        [net, tr, accu_train, accu_val] = train_batch(n, xtrain, ytrain, xval,...
-            yval, 1000, reg);
-        fprintf("Reg: %f\nTraining Acc: %f, Val Acc: %f\n", ...
-            net.performParam.regularization, accu_train, accu_val);
-    end
+fprintf("\nHidden Neurions: %d\n", n)
+for reg = lst_reg
+    [net, tr, accu_train, accu_val] = train_batch(n, xtrain, ytrain, xval,...
+        yval, 1000, reg);
+    fprintf("Reg: %f\nTraining Acc: %f, Val Acc: %f\n", ...
+        net.performParam.regularization, accu_train, accu_val);
 end
 
 %% e) MLP Sequential Mode
@@ -92,17 +95,26 @@ end
 [xtrain, ytrain] = load_data(train_path,1,false);
 [xval, yval] = load_data(val_path,1,false);
 
-n = 20;
-epochs = 30;
+fprintf("\ne) Sequential Mode\n")
+epochs = 100;
 
-net = train_seq(n, xtrain, ytrain, epochs);
+for n = [1:10, 20, 50, 100]
+    [net, accu_train, accu_val] = train_seq(n, xtrain, ytrain, epochs);
 
-y_train_pred = net(xtrain);
-y_train_pred = y_train_pred >= 0.5;
-acc_train = sum(y_train_pred == ytrain)/length(ytrain);
-y_val_pred = net(xval);
-y_val_pred = y_val_pred >= 0.5;
-acc_val = sum(y_val_pred == yval)/length(yval);
+    y_train_pred = net(xtrain);
+    y_train_pred = y_train_pred >= 0.5;
+    acc_train = sum(y_train_pred == ytrain)/length(ytrain);
+    y_val_pred = net(xval);
+    y_val_pred = y_val_pred >= 0.5;
+    acc_val = sum(y_val_pred == yval)/length(yval);
+    fprintf("# of hidden neurons:%d\n",n);
+    fprintf("acc_train: %f, acc_val: %f\n",acc_train, acc_val);
+end
+
+%%
+plot(1:1:length(accu_train), accu_train, 'b')
+hold on;
+plot(1:1:length(accu_val), accu_val, 'r')
 
 %% Functions
 function [V, L] = load_data(path, resize_ratio, apply_pca)
@@ -146,16 +158,14 @@ function [net, tr, accu_train, accu_val] = train_batch(n, xtrain, ytrain, xval,.
     net = patternnet(n);
 
     net = configure(net, xtrain, ytrain);
-    net.divideFcn= 'dividerand';
+%     net.trainFcn = 'traingdx';
     net.performParam.regularization = reg;
-    net.trainFcn = 'traingdx';
-    net.trainParam.epochs = epochs;
-    net.trainParam.max_fail = 20;
-    net.divideParam.valRatio = 0.1;
-    net.divideParam.testRatio = 0;
+%     net.trainParam.epochs = epochs;
+%     net.divideParam.testRatio = 0;
+%     net.divideParam.valRatio = 0.3;
 
     [net, tr] = train(net, xtrain, ytrain);
-    
+
     % train accu
     ytrain_pred = net(xtrain);
     ytrain_pred = ytrain_pred >= 0.5;
@@ -166,32 +176,30 @@ function [net, tr, accu_train, accu_val] = train_batch(n, xtrain, ytrain, xval,.
     accu_val = sum(ytest_pred == yval)/length(yval);
 end
 
-function net = train_seq(n, images, labels, epochs)
+function [net, accu_train, accu_val] = train_seq(n, images, labels, epochs)
     % 1. Change the input to cell array form for sequential training
     images_c = num2cell(images, 1);
     labels_c = num2cell(labels, 1);
-    train_num = length(labels)
+    train_num = length(labels);
     
     % 2. Construct and configure the MLP
     net = patternnet(n);
     
     net.divideFcn = 'dividetrain';
-    net.performParam.regularization = 0.25;
     net.trainFcn = 'traingdx';
-    net.trainParam.epochs = epochs;
-    
+
     accu_train = zeros(epochs,1);
     accu_val = zeros(epochs,1);
 
     for i = 1: epochs
         display(['Epoch: ', num2str(i)])
         idx = randperm(train_num);
-        
         net = adapt(net, images_c(:,idx), labels_c(:,idx));
-        
-        pred_train=round(net(images(:,1:train_num)));
-        accu_train(i) = 1 - mean(abs(pred_train-labels(1:train_num)));
-        pred_val=round(net(images(:,train_num+1:end)));
-        accu_val(i) = 1 - mean(abs(pred_val-labels(train_num+1:end)));
+%         pred_train=round(net(images(:,1:train_num)));
+%         accu_train(i) = mean(abs(pred_train-labels(1:train_num)));
+%         pred_val=round(net(images(:,train_num+1:end)));
+%         accu_val(i) = mean(abs(pred_val-labels(train_num+1:end)));
+%         fprintf("[Epoch: %d]\nTraining Acc: %f, Val Acc: %f\n", ...
+%         i, accu_train(i), accu_val(i));
     end
 end
